796320519849edd9f8a2462d3bdd9e75ed605e0f
Yushabh Dhande <yush2428@gmail.com>
2025-03-20 11:22:19 +0530

    update: removed extra folders and files.

--- app/(auth)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-const LoginPage = () => {
-    return (
-        <div>
-            This is Login Page without /auth i.e. parent folder !
-        </div>
-    );
-}
-
-export default LoginPage;
--- app/_components/page.tsx
+++
@@ -1,9 +0,0 @@
-const ComponentsPage = () => {
-    return (
-        <div className="bg-green-500 text-red-600">
-            This is Components Page !
-        </div>        
-    );
-}
-
-export default ComponentsPage;
