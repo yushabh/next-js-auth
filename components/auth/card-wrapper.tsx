c567794c0de8f13d006340dd510635a031bccb0d
Yushabh Dhande <yush2428@gmail.com>
2025-03-20 18:48:24 +0530

    feat: Auth components added.
    - card-wrapper
    - header
    - login-form
    - social

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { 
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader
+ } from "@/components/ui/card";
+import { Header } from "@/components/auth/header";
+import { Social } from "@/components/auth/social";
+
+interface CardWrapperProps {
+    children: React.ReactNode;
+    headerLabel: string;
+    backButtonLabel: string;
+    backButtonHref: string;
+    showSocial?: boolean;
+};
+
+export const CardWrapper = ({
+    children,
+    headerLabel,
+    backButtonLabel,
+    backButtonHref,
+    showSocial = true
+}: CardWrapperProps) => {
+    return (
+        <Card className="w-[400px] shadow-md">
+            <CardHeader>
+                <Header label={headerLabel} />
+            </CardHeader>
+            <CardContent>
+                {children}
+            </CardContent>
+            {showSocial && (
+                <CardFooter className="pt-6">
+                    <Social />
+                </CardFooter>
+            )}
+        </Card>
+    );
+};
