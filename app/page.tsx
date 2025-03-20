78054bf3bc25899463686fc38e045b2c5d79904d
Yushabh Dhande <yush2428@gmail.com>
2025-03-20 15:13:53 +0530

    feat: Landing page design added with Sign in Button component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+});
 
 export default function Home() {
   return (
-      <Button size={"lg"} variant={"outline"}>
-        Click me
-      </Button>
+      // <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <main className="flex h-full flex-col items-center justify-center bg-green-200">
+        <div className="space-y-6 text-center">
+          <h1 className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className,
+            )}>
+            🔐 Auth
+          </h1>
+          <p className="text-white text-xl">
+            A simple authentication service.
+          </p>
+          <div>
+            <Button variant={"secondary"} size={"lg"}>
+              Sign In
+            </Button>
+          </div>
+        </div>
+      </main>
   );
 }
